b1165f650b20f496322e9037494ec4ac44add951
Mitch Arndt <arndt20@purdue.edu>
2020-10-08 22:03:24 -0400

    working initial sd card datalogger code

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,99 @@
 /*
- * Blink
- * Turns on an LED on for one second,
- * then off for one second, repeatedly.
+  SD card datalogger
+ 
+ This example shows how to log data from three analog sensors 
+ to an SD card using the SD library.
+ 	
+ The circuit:
+ * analog sensors on analog ins 0, 1, and 2
+ * SD card attached to SPI bus as follows:
+ ** MOSI - pin 11, pin 7 on Teensy with audio board
+ ** MISO - pin 12
+ ** CLK - pin 13, pin 14 on Teensy with audio board
+ ** CS - pin 4,  pin 10 on Teensy with audio board
+ 
+ created  24 Nov 2010
+ modified 9 Apr 2012
+ by Tom Igoe
+ 
+ This example code is in the public domain.
+ 	 
  */
 
-#include <Arduino.h>
+#include <SD.h>
+#include <SPI.h>
 
-void blink(int num, int dur);
+// On the Ethernet Shield, CS is pin 4. Note that even if it's not
+// used as the CS pin, the hardware CS pin (10 on most Arduino boards,
+// 53 on the Mega) must be left as an output or the SD library
+// functions will not work.
+
+// change this to match your SD shield or module;
+// Arduino Ethernet shield: pin 4
+// Adafruit SD shields and modules: pin 10
+// Sparkfun SD shield: pin 8
+// Teensy audio board: pin 10
+// Teensy 3.5 & 3.6 & 4.1 on-board: BUILTIN_SDCARD
+// Wiz820+SD board: pin 4
+// Teensy 2.0: pin 0
+// Teensy++ 2.0: pin 20
+const int chipSelect = BUILTIN_SDCARD;
 
 void setup()
 {
-	// initialize LED digital pin as an output.
-	pinMode(LED_BUILTIN, OUTPUT);
+	//UNCOMMENT THESE TWO LINES FOR TEENSY AUDIO BOARD:
+	//SPI.setMOSI(7);  // Audio shield has MOSI on pin 7
+	//SPI.setSCK(14);  // Audio shield has SCK on pin 14
+
+	// Open serial communications and wait for port to open:
+	Serial.begin(9600);
+	while (!Serial)
+	{
+		; // wait for serial port to connect.
+	}
+
+	Serial.print("Initializing SD card...");
+
+	// see if the card is present and can be initialized:
+	if (!SD.begin(chipSelect))
+	{
+		Serial.println("Card failed, or not present");
+		// don't do anything more:
+		return;
+	}
+	Serial.println("card initialized.");
 }
 
 void loop()
 {
-	blink(3, 150);
+	// make a string for assembling the data to log:
+	String dataString = "";
 
-	blink(1, 1000);
-}
-
-void blink(int num, int dur)
-{
-	for (int i = 0; i < num; i++)
+	// read three sensors and append to the string:
+	for (int analogPin = 0; analogPin < 3; analogPin++)
 	{
-		digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
-		delay(dur);						 // wait for a second
-		digitalWrite(LED_BUILTIN, LOW);	 // turn the LED off by making the voltage LOW
-		delay(dur);						 // wait for a second
+		int sensor = analogRead(analogPin);
+		dataString += String(sensor);
+		if (analogPin < 2)
+		{
+			dataString += ",";
+		}
+	}
+
+	// open the file.
+	File dataFile = SD.open("datalog.txt", FILE_WRITE);
+
+	// if the file is available, write to it:
+	if (dataFile)
+	{
+		dataFile.println(dataString);
+		dataFile.close();
+		// print to the serial port too:
+		Serial.println(dataString);
+	}
+	// if the file isn't open, pop up an error:
+	else
+	{
+		Serial.println("error opening datalog.txt");
 	}
 }
