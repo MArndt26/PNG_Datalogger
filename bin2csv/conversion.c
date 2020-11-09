012d1f8198f15afe29ab665453778961c54d28f4
Mitch <mitchaarndt@gmail.com>
2020-11-09 14:57:15 -0500

    updated conversions.c

--- bin2csv/conversion.c
+++ bin2csv/conversion.c
@@ -4,6 +4,7 @@
 #include <string.h>
 
 #define CREATE_INT_FILE
+// #define CREATE_VOLTAGE_FILE
 
 /**CONVERTS BINARY FILES TO CSV DATA
  * 
@@ -32,6 +33,7 @@ struct printBuf
 
 int main(int argc, char *argv[])
 {
+#if defined(CREATE_VOLTAGE_FILE) || defined(CREATE_INT_FILE)
     if (argc != 2)
     {
         printf("Incorrect number of arguments!\n");
@@ -44,7 +46,10 @@ int main(int argc, char *argv[])
 
     char *filename = argv[1];
     FILE *inFilePtr;
+
+#ifdef CREATE_VOLTAGE_FILE
     FILE *outFilePtr;
+#endif
 
 #ifdef CREATE_INT_FILE
     FILE *intOutFilePtr;
@@ -62,9 +67,11 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
+#ifdef CREATE_VOLTAGE_FILE
     sprintf(fileWithExtension, "%s.csv", filename);
     outFilePtr = fopen(fileWithExtension, "w");
     printf("...Created %s\n", fileWithExtension);
+#endif
 
 #ifdef CREATE_INT_FILE
     sprintf(fileWithExtension, "%s(int).csv", filename);
@@ -72,59 +79,92 @@ int main(int argc, char *argv[])
     printf("...Created %s\n", fileWithExtension);
 #endif
 
-    int cols = 0;
     int numCols = ADC_CHAN;
+#ifdef CREATE_VOLTAGE_FILE
     float max_val = 4096.0 - 1; //max value is max_val - 1
     float voltageRef = 3.3;
+#endif
 
     for (int i = 0; i < numCols; i++)
     {
         if (i == 0)
         {
+#ifdef CREATE_VOLTAGE_FILE
             fprintf(outFilePtr, "%10s, ", "time (us)");
+            fprintf(outFilePtr, "%10s, ", "dt (us)");
+#endif
 #ifdef CREATE_INT_FILE
             fprintf(intOutFilePtr, "%10s, ", "time (us)");
+            fprintf(intOutFilePtr, "%10s, ", "dt (us)");
 #endif
         }
+#ifdef CREATE_VOLTAGE_FILE
+
         fprintf(outFilePtr, "%8d, ", i);
+#endif
 #ifdef CREATE_INT_FILE
         fprintf(intOutFilePtr, "%4d, ", i);
 #endif
     }
 
+#ifdef CREATE_VOLTAGE_FILE
     fprintf(outFilePtr, "\n");
+#endif
 #ifdef CREATE_INT_FILE
     fprintf(intOutFilePtr, "\n"); //write integer
 #endif
 
     struct printBuf pBuf;
 
+    int numDeletes = 0;
+    int prevTime = 0;
+
     while (fread(&pBuf, sizeof(struct printBuf), 1, inFilePtr))
     {
         for (int i = 0; i < PRINT_BUF_MULT; i++)
         {
-#ifdef CREATE_INT_FILE
-            fprintf(intOutFilePtr, "%10u, ", pBuf.time[i]);
-            for (int j = 0; j < ADC_CHAN; j++)
-            {
-                fprintf(intOutFilePtr, "%4d, ", pBuf.data[i][j]);
-            }
-            fprintf(intOutFilePtr, "\n");
-#endif
-            fprintf(outFilePtr, "%10u, ", pBuf.time[i]);
-            for (int j = 0; j < ADC_CHAN; j++)
-            {
-                float voltage = pBuf.data[i][j] / max_val * voltageRef; //calculate voltage
-                fprintf(outFilePtr, "%4.6f, ", voltage);
-            }
-            fprintf(outFilePtr, "\n");
+            int curTime = pBuf.time[i];
 
-            cols++;
+            if (curTime != 0)
+            {
+
+#ifdef CREATE_INT_FILE
+                fprintf(intOutFilePtr, "%10u, ", curTime);
+                fprintf(intOutFilePtr, "%10d, ", curTime - prevTime);
+                for (int j = 0; j < ADC_CHAN; j++)
+                {
+                    fprintf(intOutFilePtr, "%4d, ", pBuf.data[i][j]);
+                }
+                fprintf(intOutFilePtr, "\n");
+#endif
+
+#ifdef CREATE_VOLTAGE_FILE
+                fprintf(outFilePtr, "%10u, ", curTime);
+                fprintf(outFilePtr, "%10d, ", curTime - prevTime);
+                for (int j = 0; j < ADC_CHAN; j++)
+                {
+                    float voltage = pBuf.data[i][j] / max_val * voltageRef; //calculate voltage
+                    fprintf(outFilePtr, "%4.6f, ", voltage);
+                }
+                fprintf(outFilePtr, "\n");
+#endif
+
+                prevTime = pBuf.time[i];
+            }
+            else
+            {
+                numDeletes++;
+            }
         }
     }
 
+    printf("Number of rows deleted: %d\n", numDeletes);
+
     fclose(inFilePtr);
+
+#ifdef CREATE_VOLTAGE_FILE
     fclose(outFilePtr);
+#endif
 
 #ifdef CREATE_INT_FILE
     fclose(intOutFilePtr);
@@ -135,4 +175,11 @@ int main(int argc, char *argv[])
     printf("Finished writing files\n");
 
     return 0;
+
+#else
+    printf("*********************WARNING*************************\n");
+    printf("***Must Define Output file in conversion.c defines***\n");
+    printf("*****************************************************\n");
+    exit(1);
+#endif
 }
